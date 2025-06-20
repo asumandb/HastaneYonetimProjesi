f8052875a569651ff39c7809b916303073d73d7e
asumandb <asumanozsoy54@gmail.com>
2025-06-20 13:59:54 +0300

    veritabanı oluşturuldu

--- hastane_yonetim/settings.py
+++ hastane_yonetim/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hastane_yonetim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hastane_yonetimi',
+        'USER': 'root',
+        'PASSWORD': '',  # XAMPP'da genelde boştur
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
